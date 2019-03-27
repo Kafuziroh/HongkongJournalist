6934b96c86bf30e25fb1233634e31031d9ff7c34
Kafuziroh <kafuziroh@outlook.com>
2019-03-27 09:46:58 +0800

    读取文件增加位置可选项

--- HongkongJournalist/ReaderFromFiles.cpp
+++ HongkongJournalist/ReaderFromFiles.cpp
@@ -4,24 +4,51 @@
 using namespace std;
 extern int SIZE_OF_ARRAY; //全局数组大小
 
-double* ReadFromFiles() 
+double* ReadFromFiles()
 {
 	cout << "==============================================" << endl;
 	cout << "==================从文件读入==================" << endl;
 	cout << "==============================================" << endl;
 	FILE *fp = NULL;
 RESETMARK2: //文件错误重置位置
-	cout << "即将从程序同目录下读入\"array.txt\"，按回车确认...";
-	getchar();
-	getchar();
-	fp = fopen("D:\\array.txt", "r");
+	cout << "1. 从程序同目录赌读入\"array.txt\"源数据" << endl;
+	cout << "2. 读入指定文件" << endl;
+	cout << "==============================================" << endl;
+	cout << "请选择读入文件的方法：";
+	int chooserTMP = 0;
+RESETMARKFILE: //选择错误重置位置
+	cin >> chooserTMP;
+	if (chooserTMP != 1 && chooserTMP != 2)
+	{
+		getchar();
+		cout << "==============================================" << endl;
+		cout << "选择错误，请重新输入：";
+		goto RESETMARKFILE;
+	}
+	else if (chooserTMP == 1) //从程序同目录赌读入"array.txt"源数据
+	{
+		cout << "即将从程序同目录下读入\"array.txt\"，按回车确认...";
+		getchar();
+		getchar();
+		fp = fopen("D:\\array.txt", "r");
+	}
+	else if (chooserTMP == 2) //读入指定文件
+	{
+		cout << "==============================================" << endl;
+		cout << "请输入所需读取文件的完整目录：" << endl;
+		getchar();
+		char arraTemp[100];
+		gets_s(arraTemp, 100);
+		fp = fopen(arraTemp, "r");
+	}
+
 	if (!fp)
 	{
-		cout << "打开文件失败，请检查是否有\"array.txt\"位于同目录下" << endl;
+		cout << "打开文件失败，请重新选择" << endl;
 		cout << "==============================================" << endl;
 		goto RESETMARK2;
 	}
-	std::vector<double> *arrayTemp = new vector<double>; //使用动态大小的vector容器实现从文件中读取不定数量的数据
+ 	std::vector<double> *arrayTemp = new vector<double>; //使用动态大小的vector容器实现从文件中读取不定数量的数据
 	while (!feof(fp)) {
 		double tmp = 0;
 		fscanf(fp, "%lf", &tmp);
